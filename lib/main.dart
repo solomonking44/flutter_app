5ad51cec8e29f8f62aa778014181621e5eaa8d7f
solomonking44 <mugishasolomon520@gmail.com>
2022-08-24 07:38:08 -0500

    half lecture

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,20 @@
+import "package:flutter/material.dart";
 
+void main() => runApp(Rema());
+
+class Rema extends StatelessWidget {
+  static const String _title = "Rema";
+  static const String _defaultText = "This is my default text";
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(_title),
+        ),
+        body: const Text(_defaultText),
+      ),
+    );
+  }
+}
