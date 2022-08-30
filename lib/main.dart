d9e57b5a6782f4f4bd0d9934b19c9bdf97759d61
solomonking44 <mugishasolomon520@gmail.com>
2022-08-30 15:33:43 -0500

    latest changes

--- lib/main.dart
+++ lib/main.dart
@@ -8,13 +8,33 @@ class Rema extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var listOne = [
+      'what\'s your favourite color?',
+      'What\'s your favourite animal',
+    ];
     return MaterialApp(
+      title: 'Welcome to flutter',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text(_title),
+          title: const Text('Hello world'),
         ),
-        body: const Text(_defaultText),
+        body: Column(
+          // ignore: prefer_const_literals_to_create_immutables
+          children: [
+            const Text("List item 1"),
+            const Text("List item 2"),
+            const Text("List item 3"),
+            const ElevatedButton(onPressed: display()),
+          ],
+          //   Text('What is your favourite animal?'),
+          //   RaisedButton(child: Text('one'),
+          //   RaisedButton(child: Text('two'),
+          //   RaisedButton(child: Text('three'),
+          // ];
+        ),
       ),
     );
   }
+
+  void display() => const Text("It Worked!");
 }
